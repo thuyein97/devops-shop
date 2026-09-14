850bcf670232d1f24da0144864b3c741f6a95df9
thuyein <thuyein9717@gmail.com>
2026-09-14 21:33:27 +0700

    add kubeconfig step for prd

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "practice-eks-cluster"
-  cluster_version = "1.30"
+  cluster_version = "1.36"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
